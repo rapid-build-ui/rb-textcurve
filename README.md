c8099153374acedc2478fd03559d89cbd0438424
Judd Younce <jyounce@yahoo.com>
2019-04-13 20:11:04 -0500

    feat(rb-textcurve): init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# rb-textcurve
-Rapid Build UI · Web Component &lt;rb-textcurve>
+# &lt;rb-textcurve&gt; web component
+&#9874; Work in progress. Not for use yet.
+
+## Documentation
+[Showcase Site](https://rapid-build-ui.io/)
---
+++ src/client/scripts/rb-textcurve.js
@@ -0,0 +1,23 @@
+/***********
+ * RB-TEXTCURVE
+ ***********/
+import { RbBase, props, html } from '../../rb-base/scripts/rb-base.js';
+import template                from '../views/rb-textcurve.html';
+
+export class RbTextcurve extends RbBase() {
+	/* Properties
+	 *************/
+	static get props() {
+		return {
+			kind: props.string
+		};
+	}
+
+	/* Template
+	 ***********/
+	render({ props, state }) { // :string
+		return html template;
+	}
+}
+
+customElements.define('rb-textcurve', RbTextcurve);
